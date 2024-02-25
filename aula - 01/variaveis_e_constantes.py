1cfd195d2640259ed2e4abff2181ca8b8db986e1
Jander Melo <janderwow@gmail.com>
2024-02-25 20:51:07 -0300

    variaveis e constantes

---
+++ aula - 01/variaveis_e_constantes.py
@@ -0,0 +1,9 @@
+name = 'Jander';
+age = 33;
+
+print(f'Olá meu nome é {name} e tenho {age} de idade');
+
+# convenção de constantes em python
+BRAZIL_STATES = ["AM", "SP", "RJ", "SC"];
+
+print(BRAZIL_STATES);
