05eb833eb30fe92e5bcab8c8df0b0ab188a3039e
Jander Melo <janderwow@gmail.com>
2024-02-28 10:32:48 -0300

    trabalhando com strings

---
+++ aula - 03/metodos_uteis_de_strings.py
@@ -0,0 +1,12 @@
+curso = "Python"
+
+print(curso.upper())
+print(curso.lower())
+print(curso.title())
+print(curso.strip())
+print(curso.lstrip())
+
+print(curso.center(10,"#"))
+print(".".join(curso))
+
+
