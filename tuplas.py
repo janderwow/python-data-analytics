cf4dca11fb0fe1b6d157c653d8a1ea276862ad39
Jander Melo <janderwow@gmail.com>
2024-03-10 00:13:12 -0300

    conhecendo tuplas

---
+++ tuplas.py
@@ -0,0 +1,25 @@
+frutas = ("laranja", "maca", "uva",)
+
+letras = tuple("python")
+
+numeros = tuple([1, 2, 3, 5])
+
+pais = ("Brasil",)
+
+# acessando valores
+print(frutas[0])
+print(frutas[-1])
+
+# tuplas aninhadas
+
+matriz = (
+    (1, "a", 2),
+    (3, "b", 4),
+    (6, 5, "c")
+)
+
+print(matriz[0])
+
+print(matriz[1][1])
+
+print(matriz[:2])
